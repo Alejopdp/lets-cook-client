aaec1ea0a20c318de67bc8a6b96ac030d44eb958
Alejopdp <alejoscotti@gmail.com>
2021-05-03 15:15:18 +0200

    Validacion de token en pagina login

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import LoginForm from "../components/loginForm";
+import { getToken } from "../helpers/localStorage/localStorage";
+import { verifyToken } from "../helpers/serverRequests/user";
 
 const Login = (props) => {
     return (
@@ -10,3 +12,23 @@ const Login = (props) => {
 };
 
 export default Login;
+
+export async function getInitialProps(context) {
+    const token = getToken();
+    if (!token) return { props: {} };
+
+    const res = await verifyToken(token);
+    const isTokenValid = res.status === 200;
+
+    if (!isTokenValid) {
+        clearLocalStorage();
+        return { props: {} };
+    }
+
+    return {
+        redirect: {
+            permanent: false,
+            destination: "/dashboard",
+        },
+    };
+}
