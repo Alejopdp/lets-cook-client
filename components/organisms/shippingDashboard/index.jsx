5ed2edf9bc0652324a93d2363e0f61da9f67880a
Alejopdp <alejoscotti@gmail.com>
2021-05-31 18:46:48 +0200

    Fix handleClick prop en crear zona

--- components/organisms/shippingDashboard/index.jsx
+++ components/organisms/shippingDashboard/index.jsx
@@ -1,33 +1,27 @@
 // Utils & Config
-import React from 'react'
+import React from "react";
 import { useRouter } from "next/router";
 
 // External components
-import Container from '@material-ui/core/Container'
+import Container from "@material-ui/core/Container";
 
 // Internal components
-import DashboardWithButton from '../../layout/dashboardTitleWithButton/dashboardTitleWithButton'
-import ShippingTable from './shippingTable'
+import DashboardWithButton from "../../layout/dashboardTitleWithButton/dashboardTitleWithButton";
+import ShippingTable from "./shippingTable";
 
 const ShippingDashboard = () => {
     const router = useRouter();
 
     const handleClick = () => {
-        router.push("/gestion-de-envios/crear")
-    }
+        router.push("/gestion-de-envios/crear");
+    };
 
     return (
         <Container size="md">
-            <DashboardWithButton
-                title="Zonas de envío"
-                buttonText="Crear zona de envío"
-                startIcon
-                onClick={handleClick}
-            />
-
+            <DashboardWithButton title="Zonas de envío" buttonText="Crear zona de envío" startIcon handleClick={handleClick} />
             <ShippingTable />
         </Container>
-    )
-}
+    );
+};
 
 export default ShippingDashboard;
