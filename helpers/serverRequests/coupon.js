33dbf3e223b58c34b805e8291fe6f5af7d5a3537
Alejopdp <alejoscotti@gmail.com>
2021-06-14 18:14:19 +0200

    Get coupons list request function

--- helpers/serverRequests/coupon.js
+++ helpers/serverRequests/coupon.js
@@ -16,3 +16,17 @@ export const createCoupon = async (couponData) => {
         return error.response;
     }
 };
+
+export const getCouponList = async () => {
+    try {
+        const res = await Axios({
+            method: "GET",
+            url: apiUrl,
+        });
+
+        return res;
+    } catch (error) {
+        console.log(error);
+        return error.response;
+    }
+};
