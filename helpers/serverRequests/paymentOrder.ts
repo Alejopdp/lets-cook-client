43e274e82668ebab36fba5d39f4ec46c27bb7ff3
Alejopdp <alejoscotti@gmail.com>
2021-08-09 11:06:13 +0200

    feat(serverRequest): added update payment order state endpoint

--- helpers/serverRequests/paymentOrder.ts
+++ helpers/serverRequests/paymentOrder.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { PaymentOrderState } from "helpers/types/paymentOrderState";
 const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/payment-order`;
 
 export const getPaymentOrders = async (locale: string = "es") => {
@@ -25,6 +26,23 @@ export const getPaymentOrder = async (paymentOrderId: string, locale: string = "
             url: `${apiUrl}/${paymentOrderId}`,
             params: {
                 locale,
+            },
+        });
+
+        return res;
+    } catch (error) {
+        console.log(error);
+        return error.response;
+    }
+};
+
+export const updatePaymentOrderState = async (paymentOrderId: string, state: PaymentOrderState) => {
+    try {
+        const res = await axios({
+            method: "PUT",
+            url: `${apiUrl}/update-state/${paymentOrderId}`,
+            data: {
+                state,
             },
         });
 
