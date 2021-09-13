3ec3d9d4760ca36ab2365a1bbb5a5f66988b2d81
Alejopdp <alejoscotti@gmail.com>
2021-09-13 12:23:55 +0200

    feat(paymentOrder): refunds added

--- components/organisms/paymentOrderDetail/paymentOrderGrid/refundModal/index.jsx
+++ components/organisms/paymentOrderDetail/paymentOrderGrid/refundModal/index.jsx
@@ -1,11 +1,10 @@
 // Utils & Config
-import React, { useState, useEffect } from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import React, { useState, useEffect } from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 
 // External Components
-import Modal from '../../../../atoms/modal/modal'
-import Typography from '@material-ui/core/Typography';
-
+import Modal from "../../../../atoms/modal/modal";
+import Typography from "@material-ui/core/Typography";
 
 const RefundModal = (props) => {
     const theme = useTheme();
@@ -15,16 +14,16 @@ const RefundModal = (props) => {
             open={props.open}
             handleClose={props.handleClose}
             handlePrimaryButtonClick={props.handlePrimaryButtonClick}
-            title='Reembolso'
-            primaryButtonText='reembolsar'
-            primaryButtonColor='#FC1919'
-            secondaryButtonText='cancelar'
+            title="Reembolso"
+            primaryButtonText="reembolsar"
+            primaryButtonColor="#FC1919"
+            secondaryButtonText="cancelar"
         >
-            <Typography variant='body1' color='textSecondary' style={{ fontSize: '16px' }}>
-                ¿Estás seguro de que quieres reembolsar {props.amountToRefund}?
+            <Typography variant="body1" color="textSecondary" style={{ fontSize: "16px" }}>
+                ¿Estás seguro de que quieres reembolsar <b>{props.amountToRefund} €</b>?
             </Typography>
         </Modal>
     );
-}
+};
 
 export default RefundModal;
