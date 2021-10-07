880f8dfa726de9e208d7ea585789fd378e5ebf85
Alejopdp <alejoscotti@gmail.com>
2021-10-07 15:30:13 +0200

    fix(order): diabled option for skipping billed orders

--- helpers/types/order.ts
+++ helpers/types/order.ts
@@ -4,3 +4,12 @@ export interface SkippableOrder {
     shippingDate: string;
     isSkipped: boolean;
 }
+
+export enum OrderState {
+    ORDER_ACTIVE = "ORDER_ACTIVE",
+    ORDER_SKIPPED = "ORDER_SKIPPED",
+    ORDER_CANCELLED = "ORDER_CANCELLED",
+    ORDER_BILLED = "ORDER_BILLED",
+    ORDER_PENDING_PAYMENT = "ORDER_PENDING_PAYMENT",
+    ORDER_REJECTED_PAYMENT = "ORDER_REJECTED_PAYMENT",
+}
