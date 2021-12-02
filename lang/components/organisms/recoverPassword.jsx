de900bf8c504e12b9f4eaf0a4930fe27aa27545e
Alejopdp <alejoscotti@gmail.com>
2021-12-02 10:28:07 +0100

    fix(i18n): language strucutre fixed

--- lang/components/organisms/recoverPassword.jsx
+++ lang/components/organisms/recoverPassword.jsx
@@ -1,24 +1,30 @@
 export const recoverPassword = {
     en: {
-        title: "Recover password",
-        passwordHelperText: "The password must be at least 8 characters long and contain 1 uppercase letter and 1 number",
-        paragraph: "The code is valid. Please enter below your new password:",
-        passwordInput: "Enter your password",
-        buttonText: "Continue",
-        register: {
-            text: "You don’t have an account yet?",
-            boldText: "Sign up here",
+        title: "Recuperar contraseña",
+        form: {
+            text: "Entra tu correo electrónico a continuación y le enviaremos un email con un link para que pueda ingresar su nueva contraseña",
+            emailPlaceholder: "Correo electrónico",
+            button: "RECUPERAR CONTRASEÑA",
+            login: "Volver a Iniciar sesión",
+        },
+        success: {
+            title: "Solicitud de recupero exitosa",
+            text: "Hemos enviado un email al correo electrónico ingresado para que puedas ingresar tu nueva contraseña.",
+            login: "Volver a Iniciar sesión",
         },
     },
     ca: {
-        title: "Recuperar contrasenya",
-        passwordHelperText: "La contrasenya ha de tenir almenys 8 caracters, 1 majúscula i 1 número",
-        paragraph: "El codi introduït és vàlid. Introdueix a continuació la nova contrasenya:",
-        passwordInput: "Introdueix la teva contrasenya",
-        buttonText: "Continua",
-        register: {
-            text: "Encara no tens un compte?",
-            boldText: "Registra’t aquí",
+        title: "Recuperar contraseña",
+        form: {
+            text: "Entra tu correo electrónico a continuación y le enviaremos un email con un link para que pueda ingresar su nueva contraseña",
+            emailPlaceholder: "Correo electrónico",
+            button: "RECUPERAR CONTRASEÑA",
+            login: "Volver a Iniciar sesión",
+        },
+        success: {
+            title: "Solicitud de recupero exitosa",
+            text: "Hemos enviado un email al correo electrónico ingresado para que puedas ingresar tu nueva contraseña.",
+            login: "Volver a Iniciar sesión",
         },
     },
 
