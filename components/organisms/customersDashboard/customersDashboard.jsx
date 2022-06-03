60521750bf6e26b6c495237cbf73b6344f8b49dd
Alejo Scotti <alejo@novolabs.xyz>
2022-06-03 14:34:34 +0200

    refactor: console log removed

--- components/organisms/customersDashboard/customersDashboard.jsx
+++ components/organisms/customersDashboard/customersDashboard.jsx
@@ -84,7 +84,6 @@ const CustomersDashboard = (props) => {
     useEffect(() => {
         const getCustomers = async () => {
             const res = await getCustomerList(getFromLocalStorage("token"));
-            console.log(res);
             if (res.status === 200) {
                 setCustomers(res.data);
             } else {
