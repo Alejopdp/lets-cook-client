5aaafe6542d20c2af7f5c552ae3ce43915d6d9a2
Alejo Scotti <alejo@novolabs.xyz>
2023-08-31 14:09:07 +0200

    feat: wallet movements added

---
+++ components/organisms/customerProfile/customerInfo/customerInfoModals/walletMovementsModal.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { WalletMovement } from "helpers/types/customer";
+import { Dialog, DialogContent, DialogTitle } from "@material-ui/core";
+import { format } from "date-fns";
+
+type WalletMovementsModalProps = {
+    movements: WalletMovement[];
+    isOpen: boolean;
+    handleClose: () => void;
+};
+
+const WalletMovementsModal = (props: WalletMovementsModalProps) => {
+    return (
+        <Dialog open={props.isOpen} onClose={props.handleClose} fullWidth>
+            <DialogTitle>Movimientos del Monedero</DialogTitle>
+            <DialogContent>
+                <Box display="flex" flexDirection="column">
+                    {props.movements.map((movement) => {
+                        const formattedDate = format(new Date(movement.createdAt), "dd/MM/yyyy HH:mm:ss");
+                        return (
+                            <Box display="flex" flexDirection="row" justifyContent={"space-between"} marginBottom={2}>
+                                <Typography variant="body2" color="initial">
+                                    {movement.type}
+                                </Typography>
+                                <Typography variant="body2" color="initial">
+                                    {formattedDate}
+                                </Typography>
+                            </Box>
+                        );
+                    })}
+                </Box>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default WalletMovementsModal;
